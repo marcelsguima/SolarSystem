277bb9bb904edee6d8b5ed0c7817c36c174a323b
marceslguima <marcelsguima@gmail.com>
2022-08-18 21:55:45 -0300

    css

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -8,7 +8,7 @@ class Missions extends React.Component {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
-        {missions.map(({name, year, country, destination}) => (
+        {missions.map(({ name, year, country, destination }) => (
           <MissionCard
             key={ name }
             name={ name }
--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './PlanetCard.css';
 
 class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
-        <p data-testid="planet-name">
-          {planetName}
-        </p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
-      </div>
+      <section className="card">
+        <div data-testid="planet-card">
+          <p data-testid="planet-name">
+            {planetName}
+          </p>
+          <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        </div>
+      </section>
     );
   }
 }
