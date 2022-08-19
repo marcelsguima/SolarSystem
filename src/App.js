d0b27fe2756c8814f2787239aa1b2573ff6f706d
marceslguima <marcelsguima@gmail.com>
2022-08-18 21:33:03 -0300

    Requisito 10

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 // import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
+// import Title from './components/Title';
 import Missions from './components/Missions';
 
 class App extends React.Component {
@@ -12,7 +12,7 @@ class App extends React.Component {
         <Header />
         <SolarSystem />
         <Missions />
-        <Title />
+        {/* <Title /> */}
       </div>
     );
   }
