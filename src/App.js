dcac3755b442c02ce4224a710875a356952b38a3
marcelsguima <marcelsguima@gmail.com>
2023-07-14 15:26:12 -0300

    Publish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Header from './components/Header';
-// import PlanetCard from './components/PlanetCard';
 import SolarSystem from './components/SolarSystem';
-// import Title from './components/Title';
 import Missions from './components/Missions';
+import './App.css';
 
 class App extends React.Component {
   render() {
--- src/components/PlanetCard.jsx
+++ src/components/PlanetCard.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './PlanetCard.css';
 
 class PlanetCard extends React.Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <section className="card">
+      <section className="pCard">
         <div data-testid="planet-card">
           <p data-testid="planet-name">
             {planetName}
